207103c67d88383ee9ed6336600526af0bf622fe
Beo <146191603+Hadeyoshi@users.noreply.github.com>
2023-09-27 20:04:00 +0800

    Add files via upload

---
+++ Latihan/src/latihan/Latihan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan;
+
+public class Latihan {
+
+    public static void main(String[] args) {
+          int sisi, luas;
+        sisi = 3;
+        luas = sisi*sisi;
+        System.out.println("Luas Persegi = " + luas + "cm");
+    }
+    
+}
